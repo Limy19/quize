989a94699470ce9c82a4056877596f500c8ef69f
meechisdeadd <ifuckyourgrandma@mail.ru>
2023-11-17 14:44:04 +0300

    BAZA_4

--- db/models/categoryquestion.js
+++ db/models/categoryquestion.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.TEXT,
       },
-      image: {
+      url: {
         type: DataTypes.TEXT,
       },
     },
