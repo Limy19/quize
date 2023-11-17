b11756ee36bbc0508ff00114107dd809008a0394
Lida <lidamalygina@mail.ru>
2023-11-17 14:16:56 +0300

    relise1.1

--- components/CategoryList.jsx
+++ components/CategoryList.jsx
@@ -7,6 +7,7 @@ function CategoryList({ title, categories, user }) {
   return (
     <Layout title={title}>
       <div>{`Hello,${user.name}!  Let's go! Score:${user.score}`}</div>
+      
       <div>
         {categories.map((category) => (
           <Category key={category.id} category={category} />
